44939766d6b1bf717ce0a4f1aaa0304a993919a3
sangw <sangw@DESKTOP-PTLGJUA.local>
2018-11-27 20:18:07 +0900

    알송 uml 주소 구조변경

--- Presto.SWCamp.Lyrics/AlsongLRCManager.cs
+++ Presto.SWCamp.Lyrics/AlsongLRCManager.cs
@@ -9,46 +9,63 @@ using System.Threading.Tasks;
 namespace Presto.SWCamp.Lyrics
 {
     class AlsongLRCManager
-    {
-        public void Test()
+    {//sring -> StringBuilder 변경
+        private StringBuilder data;
+        
+        AlsongLRCManager()
+        {
+            data = new StringBuilder();
+            data.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            data.Append("<SOAP-ENV:Envelope ");
+            data.Append("xlns:SOAP-ENV =\"http://www.w3.org/2003/05/soap-envelope\" ");
+            data.Append("xmlns:SOAP-ENC=\"http://wwww.3org/2003/05/soap-encoding\" ");
+            data.Append("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ");
+            data.Append("xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" ");
+            data.Append("xmlns:ns2=\"ALSongWebServer/Service1Soap\" ");
+            data.Append("xmlns:ns1=\"ALSongWebServer\" ");
+            data.Append("xmlns:ns3=\"ALSongWebServer/Service1Soap12\">");
+            data.Append("<SOAP-ENV:Body>");
+            data.Append("<ns1:GetResembleLyric2>");
+            data.Append("<ns1:stQuery>");
+            data.Append("<ns1:strTitle>lucky strike</ns1:strTitle>");
+            data.Append("<ns1:strArtistName>maroon5</ns1:strArtistName>");
+            data.Append("<ns1:nCurPage>0</ns1:nCurPage>");
+            data.Append("</ns1:stQuery>");
+            data.Append("</ns1:GetResembleLyric2>");
+            data.Append("</SOAP-ENV:Body>");
+            data.Append("</SOAP-ENV:Envelope>");
+            Console.WriteLine(RequestLRCToAlsong("비와당신", "럼블피쉬"));
+        }
+        public string RequestLRCToAlsong(string title, string artist)
         {
             string url = "http://lyrics.alsong.co.kr/alsongwebservice/service1.asmx";
-
-            string data = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-                "<SOAP-ENV:Envelope " +
-                    "xmlns:SOAP-ENV =\"http://www.w3.org/2003/05/soap-envelope\" " +
-                    "xmlns:SOAP-ENC=\"http://www.w3.org/2003/05/soap-encoding\" " +
-                    "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
-                    "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
-                    "xmlns:ns2=\"ALSongWebServer/Service1Soap\" " +
-                    "xmlns:ns1=\"ALSongWebServer\" " +
-                    "xmlns:ns3=\"ALSongWebServer/Service1Soap12\">" +
-                    "<SOAP-ENV:Body>" +
-                        "<ns1:GetResembleLyric2>" +
-                        "<ns1:stQuery>" +
-                            "<ns1:strTitle>" + "lucky strike" + "</ns1:strTitle>" +
-                            "<ns1:strArtistName>" + "maroon5" + "</ns1:strArtistName>" +
-                            "<ns1:nCurPage>0</ns1:nCurPage>" +
-                        "</ns1:stQuery>" +
-                    "</ns1:GetResembleLyric2>" +
-                    "</SOAP-ENV:Body>" +
-                "</SOAP-ENV:Envelope>";
-            Console.WriteLine(data);
-            byte[] bytes = UTF8Encoding.UTF8.GetBytes(data);
+            string response = String.Empty;
+            Console.WriteLine(String.Format(data.ToString(), title, artist));
+            byte[] bytes = UTF8Encoding.UTF8.GetBytes(String.Format(data.ToString(), title, artist));
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "POST";
             request.UserAgent = "gSOAP/2.7";
             request.ContentType = "application/soap+xml; charset=UTF-8";
 
-            Stream requestStream = request.GetRequestStream();
-            requestStream.Write(bytes, 0, bytes.Length);
-            requestStream.Close();
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(bytes, 0, bytes.Length);
+            }
 
-            HttpWebResponse httpWebResponse = (HttpWebResponse)request.GetResponse();
-            StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream(), Encoding.GetEncoding("UTF-8"));
-            string respone = streamReader.ReadToEnd();
+            using (HttpWebResponse httpWebResponse = (HttpWebResponse)request.GetResponse())
+            {
+                StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream(), Encoding.GetEncoding("UTF-8"));
+                response = streamReader.ReadToEnd();
+            }
 
-            Console.Write(respone);
+            return response;
+
+        }
+
+        public string HtmlParsingToLRC(ref string data)
+        {
+
+            return string.Empty;
         }
     }
 }
